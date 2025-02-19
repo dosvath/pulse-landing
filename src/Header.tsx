120a4e61e00835ae009b3e83fb3f6bc46f9f1eb4
Daniel <daniel@oneleet.com>
2025-02-20 00:56:55 +0400

    Finish header section

---
+++ src/Header.tsx
@@ -0,0 +1,32 @@
+import instagram from "@/assets/instagram.svg";
+import logo from "@/assets/logo-w-text.svg";
+import { Button } from "./components/ui/button";
+
+export function Header() {
+  return (
+    <div className="w-full justify-between p-4 md:pt-6 flex max-w-7xl mx-auto">
+      <img src={logo} alt="Pulse Logo" className="h-8" />
+      <div className="flex flex-row md:space-x-4 items-center">
+        <Button
+          variant="ghost"
+          className="border border-white hover:bg-white/10"
+          asChild
+        >
+          <a href="https://app.pulse-date.com">Login</a>
+        </Button>
+        <a
+          href="https://instagram.com/pulsedating"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hidden md:block"
+        >
+          <img
+            src={instagram}
+            alt="Instagram Logo"
+            className="w-6 h-6 cursor-pointer"
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
---
+++ src/VideoIntroSection.tsx
@@ -0,0 +1,78 @@
+import { Button } from "./components/ui/button";
+import { Header } from "./Header";
+import { useMediaQuery } from "./useMediaQuery";
+
+export function VideoIntroSection() {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  function DesktopText() {
+    return (
+      <div className="absolute top-1/2 left-0 transform -translate-y-1/3 w-full">
+        <div className="max-w-5xl mx-auto px-8">
+          <h1 className="text-5xl font-bold text-white mb-6">
+            No Swipes. No Games.
+          </h1>
+          <p className="text-xl text-zinc-300 mb-8">
+            Join the largest, <strong>verified</strong> community of the
+            <br />
+            most <strong>beautiful</strong> and <strong>successful</strong>{" "}
+            singles
+            <br />
+            around the world.
+          </p>
+          <Button
+            asChild
+            className="bg-white text-black h-12 text-md font-medium py-3 px-8 rounded-full hover:bg-gray-100 transition"
+          >
+            <a href="https://staging.pulse-date.com/#/sign-up">Join Pulse</a>
+          </Button>
+          <div className="mt-4 text-white">
+            Already a member?{" "}
+            <a href="https://staging.pulse-date.com/" className="underline">
+              Login here
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  function MobileText() {
+    return (
+      <div className="absolute top-3/4 left-0 transform -translate-y-1/2 w-full">
+        <div className="max-w-5xl mx-auto px-8 w-full text-center">
+          <div className="text-5xl font-bold text-white flex flex-col gap-3">
+            <div>No Swipes.</div>
+            <div> No Games.</div>
+          </div>
+          <Button
+            asChild
+            className="mt-8 bg-white text-black h-12 w-62 text-md font-medium py-3 px-8 rounded-full hover:bg-gray-100 transition"
+          >
+            <a href="https://staging.pulse-date.com/#/sign-up">Join Pulse</a>
+          </Button>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="w-full relative min-h-50">
+      <video
+        src={
+          isDesktop
+            ? "https://images.pulse-date.com/header-video-desktop.mp4"
+            : "https://images.pulse-date.com/landing-video-mobile.mp4"
+        }
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="w-full h-[90vh] object-cover opacity-45"
+      />
+      <div className="absolute top-0 left-0 w-full">
+        <Header />
+      </div>
+      {isDesktop ? <DesktopText /> : <MobileText />}
+    </div>
+  );
+}
